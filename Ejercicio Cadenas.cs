5067492d547cc89ef6975c530ab599ff17801690
María Gabriela Escobar <82680943+mariagEscobar@users.noreply.github.com>
2021-10-09 13:00:54 -0400

    Add files via upload

---
+++ Ejercicio Cadenas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EjercicioCadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" \t \t ¡Bienvenido! Ingrese una frase \n \n" );
+            
+            string cadena = Console.ReadLine();
+            string[] p = cadena.Split();
+            for (int i = 0; i < p.Length; i++)  
+            {
+                Console.WriteLine("\n \t Palabra {0} \n {1}  ",(i+1), p[i]); 
+                
+            }
+        }
+    }
+}
